d9d5c157ed15d81d6e4941e0958b69869d40d209
j.boesl <j.boesl@adito.de>
2011-07-03 21:45:17 +0200

    Tabelle übernimmt Spaltenproperties.

--- nb-adito-interface/src/main/java/de/adito/aditoweb/nbm/nbide/nbaditointerface/form/layout/INonVisualLayoutComponent.java
+++ nb-adito-interface/src/main/java/de/adito/aditoweb/nbm/nbide/nbaditointerface/form/layout/INonVisualLayoutComponent.java
@@ -1,7 +1,6 @@
 package de.adito.aditoweb.nbm.nbide.nbaditointerface.form.layout;
 
 import java.awt.*;
-import java.beans.PropertyChangeListener;
 
 /**
  * @author J. Boesl, 29.06.11
@@ -12,8 +11,6 @@ public interface INonVisualLayoutComponent
   void addNonVisComp(Object pNonVisualComponent);
 
   void removeNonVisComp(Object pNonVisualComponent);
-
-  void addPropertyChangeListener(PropertyChangeListener pPropertyChangeListener);
 
   Rectangle getBounds(Object pNonVisualComponent);
 
