fa5b9f34178aabd0ae65b40f50709dc9e5a85b1d
j.boesl <j.boesl@adito.de>
2012-09-10 09:26:12 +0200

    NB72: MimeResolver warnings entfernt.

---
+++ org-netbeans-modules-form/src/main/java/org/netbeans/modules/form/adito/mimresolvers/form/package-info.java
@@ -0,0 +1,6 @@
+@NbBundle.Messages("FORM_RESOLVER_NAME=FormResolver.xml")
+@MIMEResolver.Registration(resource = "FormResolver.xml", position = 137, displayName = "#FORM_RESOLVER_NAME")
+package org.netbeans.modules.form.adito.mimresolvers.form;
+
+import org.openide.filesystems.MIMEResolver;
+import org.openide.util.NbBundle;
---
+++ org-netbeans-modules-form/src/main/java/org/netbeans/modules/form/adito/mimresolvers/palette/package-info.java
@@ -0,0 +1,6 @@
+@NbBundle.Messages("SWING_PALETTE_ITEM_RESOLVER_NAME=SwingPaletteItemResolver.xml")
+@MIMEResolver.Registration(resource = "PaletteItemResolver.xml", position = 50037, displayName = "#SWING_PALETTE_ITEM_RESOLVER_NAME")
+package org.netbeans.modules.form.adito.mimresolvers.palette;
+
+import org.openide.filesystems.MIMEResolver;
+import org.openide.util.NbBundle;
