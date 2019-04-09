9b9cdab48e21183798b492e2ab8a1a58eb4bf7fd
t.tasior <t.tasior@adito.de>
2019-04-09 08:36:56 +0200

    Unterstützung Liquibase Plugin.

---
+++ nb-adito-interface/src/main/java/de/adito/aditoweb/nbm/nbide/nbaditointerface/liquibase/ILiquibaseActionFolder.java
@@ -0,0 +1,8 @@
+package de.adito.aditoweb.nbm.nbide.nbaditointerface.liquibase;
+
+/**
+ * @author t.tasior, 01.04.2019
+ */
+public interface ILiquibaseActionFolder
+{
+}
---
+++ nb-adito-interface/src/main/java/de/adito/aditoweb/nbm/nbide/nbaditointerface/liquibase/LiquiConstants.java
@@ -0,0 +1,18 @@
+package de.adito.aditoweb.nbm.nbide.nbaditointerface.liquibase;
+
+/**
+ * @author t.tasior, 01.04.2019
+ */
+public final class LiquiConstants
+{
+  public final static String LIQUIBASE_FOLDER = ".liquibase";
+  public final static String CHANGELOG_FILE = "changelog.xml";
+  public final static String ACTION_FOLDER = "Adito/Liquibase/Actions";
+  public final static String ACTION_REFERENCE = "Actions/Project/Liquibase/XML";
+  public final static String DETAILS_ACTION_KEY = "AditoDetailsActionKey";
+  public final static String CHANGELOG_KEY = "ChangelogKey";
+
+  private LiquiConstants()
+  {
+  }
+}
