0514a55d15cc71061200db1c5564d8420d4464b3
Werner Glanzer <w.glanzer@adito.de>
2019-06-18 13:36:13 +0200

    Added second method to IFileConverters, to convert with Readers and Writers

--- nb-adito-interface/src/main/java/de/adito/aditoweb/nbm/nbide/nbaditointerface/conversions/IFileConverter.java
+++ nb-adito-interface/src/main/java/de/adito/aditoweb/nbm/nbide/nbaditointerface/conversions/IFileConverter.java
@@ -1,9 +1,8 @@
 package de.adito.aditoweb.nbm.nbide.nbaditointerface.conversions;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.*;
 
-import java.io.File;
+import java.io.*;
 import java.util.Map;
 
 /**
@@ -36,6 +35,20 @@ public interface IFileConverter
    */
   @Nullable
   Object convert(@NotNull File pSourceLocation, @NotNull File pTargetLocation, @NotNull String pSourceType, @NotNull String pTargetType,
+                 @NotNull Map<Object, Object> pParams);
+
+  /**
+   * Converts the source file to the specified type at the target location
+   *
+   * @param pSource     Reader for the input source
+   * @param pTarget     Write for the output
+   * @param pSourceType File type of the file to convert
+   * @param pTargetType File type of the final file
+   * @param pParams     Parameters for the conversion
+   * @return conversion result, or null
+   */
+  @Nullable
+  Object convert(@NotNull Reader pSource, @NotNull Writer pTarget, @NotNull String pSourceType, @NotNull String pTargetType,
                  @NotNull Map<Object, Object> pParams);
 
 }
