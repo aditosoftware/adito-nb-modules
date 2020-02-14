0cb609f9ceec06851264bc833f3bb4dd4ab4defa
m.kaspera <m.kaspera@adito.de>
2020-02-14 09:15:01 +0100

    The PopupWindow is now only automatically disposed if any mouse/Key/Focusevents happen outside the window or its components, can also be manually disposed

--- de-adito-netbeans-utilities/src/main/java/de/adito/swing/popup/PopupWindow.java
+++ de-adito-netbeans-utilities/src/main/java/de/adito/swing/popup/PopupWindow.java
@@ -1,11 +1,8 @@
 package de.adito.swing.popup;
 
 import javax.swing.*;
-import java.awt.AWTEvent;
-import java.awt.Toolkit;
-import java.awt.Window;
-import java.awt.event.AWTEventListener;
-import java.awt.event.WindowEvent;
+import java.awt.*;
+import java.awt.event.*;
 
 /**
  * a implementation of the HeavyWightWindow to show all branches
@@ -41,20 +38,68 @@ public class PopupWindow extends JWindow
     SwingUtilities.invokeLater(() -> Toolkit.getDefaultToolkit().addAWTEventListener(windowDisposer, AWTEvent.WINDOW_EVENT_MASK | AWTEvent.KEY_EVENT_MASK));
   }
 
+  public void disposeWindow() {
+    Toolkit.getDefaultToolkit().removeAWTEventListener(windowDisposer);
+    dispose();
+  }
+
+  /**
+   * AWTEventListener that disposes of the window if any mouse/keyboard/windowAction happens outside this component or any of its children
+   */
   private class _WindowDisposer implements AWTEventListener
   {
 
     @Override
-    public void eventDispatched(AWTEvent event)
+    public void eventDispatched(AWTEvent pEvent)
     {
-      if (event.getID() != WindowEvent.WINDOW_OPENED)
+      if (!_isSourceRecursive(pEvent, PopupWindow.this))
       {
-        if (event.getSource() != PopupWindow.this || !((WindowEvent) event).getWindow().getType().equals(Type.POPUP))
+        if (pEvent instanceof MouseEvent && (pEvent.getID() == MouseEvent.MOUSE_CLICKED || pEvent.getID() == MouseEvent.MOUSE_WHEEL))
         {
-          Toolkit.getDefaultToolkit().removeAWTEventListener(this);
-          dispose();
+          disposeWindow();
+        }
+        if (pEvent instanceof WindowEvent && pEvent.getID() != WindowEvent.WINDOW_OPENED
+            && !(pEvent.getID() == WindowEvent.WINDOW_CLOSED && pEvent.getSource() instanceof PopupWindow))
+        {
+          disposeWindow();
+        }
+        if (pEvent instanceof KeyEvent)
+        {
+          disposeWindow();
         }
       }
+      else if (pEvent instanceof KeyEvent && ((KeyEvent) pEvent).getKeyCode() == KeyEvent.VK_ESCAPE)
+      {
+        disposeWindow();
+      }
+    }
+
+    /**
+     * removes the AWTEventListener and disposes of the ChunkPopupWindow
+     */
+    void disposeWindow()
+    {
+      Toolkit.getDefaultToolkit().removeAWTEventListener(this);
+      dispose();
+    }
+
+    /**
+     * Checks if the container or any of its components (or the components components or...)
+     * is the source of the event.
+     * This is a recursive function
+     *
+     * @param pAWTEvent  event to look for
+     * @param pContainer Container to check
+     * @return true if event originated in the Container or any of its children, false otherwise
+     */
+    private boolean _isSourceRecursive(AWTEvent pAWTEvent, Container pContainer)
+    {
+      for (Component component : pContainer.getComponents())
+      {
+        if (pAWTEvent.getSource() == component || (component instanceof Container && _isSourceRecursive(pAWTEvent, (Container) component)))
+          return true;
+      }
+      return false;
     }
   }
 }
