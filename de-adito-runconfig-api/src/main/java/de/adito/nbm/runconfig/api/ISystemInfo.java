6bb689f5e272823917139278ada50699e88e64f6
m.kaspera <m.kaspera@adito.de>
2020-07-24 10:28:32 +0200

    The ISystemInfo now also requires a getProject() method and provides some keys for the parameters map

--- de-adito-runconfig-api/src/main/java/de/adito/nbm/runconfig/api/ISystemInfo.java
+++ de-adito-runconfig-api/src/main/java/de/adito/nbm/runconfig/api/ISystemInfo.java
@@ -2,6 +2,7 @@ package de.adito.nbm.runconfig.api;
 
 import de.adito.aditoweb.nbm.nbide.nbaditointerface.tunnel.ITunnelConfigProvider;
 import io.reactivex.rxjava3.core.Observable;
+import org.netbeans.api.project.Project;
 
 import java.util.Map;
 
@@ -12,6 +13,11 @@ import java.util.Map;
  */
 public interface ISystemInfo extends ITunnelConfigProvider
 {
+
+  String WEBCLIENT_URL_KEY = "neonWebClientUrl";
+  String TELNET_LOGGING_ENABLED_KEY = "loggingTelnetEnabled";
+  String TELNET_HOST_EXTERNAL_ADRESS_KEY = "loggingTelnetExternalAddress";
+  String TELNET_PORT_KEY = "loggingTelnetPort";
 
   /**
    *
@@ -33,5 +39,12 @@ public interface ISystemInfo extends ITunnelConfigProvider
    * @return Map
    */
   Map<String, String> getParameters();
+
+  /**
+   * Get the project that this System belongs to
+   *
+   * @return Project
+   */
+  Project getProject();
 
 }
