c1610fd08cb3125e9017808093e82d9a9180d82d
m.kaspera <m.kaspera@adito.de>
2020-10-28 08:47:59 +0100

    Adjustments to the diff interface

--- nb-adito-interface/src/main/java/de/adito/aditoweb/nbm/nbide/nbaditointerface/git/IDiffService.java
+++ nb-adito-interface/src/main/java/de/adito/aditoweb/nbm/nbide/nbaditointerface/git/IDiffService.java
@@ -1,5 +1,7 @@
 package de.adito.aditoweb.nbm.nbide.nbaditointerface.git;
 
+import org.jetbrains.annotations.*;
+
 /**
  * Defines methods for a service that provides capabilities for showing the differences between two Strings in a dialog
  *
@@ -11,18 +13,43 @@ public interface IDiffService
   /**
    * Compares two Strings and then displays a Dialog that shows the differences of the two Versions
    *
-   * @param pVersion1 String to be compared to pVersion2
-   * @param pVersion2 String to be compared to pVersion1
+   * @param pVersionLeft String to be compared to pVersionRight, will be displayed on the left side of the comparison
+   * @param pVersionRight String to be compared to pVersionLeft, will be displayed on the right side of the comparison
    */
-  void showDiff(String pVersion1, String pVersion2);
+  void showDiff(@NotNull String pVersionLeft, @NotNull String pVersionRight);
 
   /**
-   * Compares two byte Arrays (assumed to represent Strings) by converting the byte arrays to Strings (should include encoding detection), compares the Strings
-   * and then displays a Dialog that shows the differences of the two Versions
+   * Compares two byte Arrays (assumed to represent Strings) by converting the byte arrays to Strings (should include encoding detection),
+   * compares the Strings and then displays a Dialog that shows the differences of the two Versions
    *
-   * @param pVersion1 byte array to be compared to pVersion2
-   * @param pVersion2 byte array to be compared to pVersion1
+   * @param pVersionLeft byte array to be compared to pVersionRight, will be displayed on the left side of the comparison
+   * @param pVersionRight byte array to be compared to pVersionLeft, will be displayed on the right side of the comparison
    */
-  void showDiff(byte[] pVersion1, byte[] pVersion2);
+  void showDiff(@NotNull byte[] pVersionLeft, @NotNull byte[] pVersionRight);
+
+  /**
+   * Compares two Strings and then displays a Dialog that shows the differences of the two Versions
+   *
+   * @param pVersionLeft  String to be compared to pVersionRight, will be displayed on the left side of the comparison
+   * @param pVersionRight String to be compared to pVersionLeft, will be displayed on the right side of the comparison
+   * @param pTitle        Will be used as title for the dialog
+   * @param pHeaderLeft   Used to to give an origin to the left side of the comparison (e.g. commit id, file name or the like)
+   * @param pHeaderRight  Used to to give an origin to the right side of the comparison (e.g. commit id, file name or the like)
+   */
+  void showDiff(@NotNull String pVersionLeft, @NotNull String pVersionRight, @NotNull String pTitle, @Nullable String pHeaderLeft,
+                @Nullable String pHeaderRight);
+
+  /**
+   * Compares two byte Arrays (assumed to represent Strings) by converting the byte arrays to Strings (should include encoding detection),
+   * compares the Strings and then displays a Dialog that shows the differences of the two Versions
+   *
+   * @param pVersionLeft  byte array to be compared to pVersionRight
+   * @param pVersionRight byte array to be compared to pVersionLeft
+   * @param pTitle        Will be used as title for the dialog
+   * @param pHeaderLeft   Used to to give an origin to the left side of the comparison (e.g. commit id, file name or the like)
+   * @param pHeaderRight  Used to to give an origin to the right side of the comparison (e.g. commit id, file name or the like)
+   */
+  void showDiff(@NotNull byte[] pVersionLeft, @NotNull byte[] pVersionRight, @NotNull String pTitle, @Nullable String pHeaderLeft,
+                @Nullable String pHeaderRight);
 
 }
