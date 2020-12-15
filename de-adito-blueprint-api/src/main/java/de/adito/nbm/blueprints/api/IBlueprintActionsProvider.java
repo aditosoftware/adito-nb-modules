0c62ce0938837259c075e10470939cbac0706ae5
Stefan Seemann <s.seemann@adito.de>
2020-12-15 09:28:58 +0100

    feat: add interface IBlueprintActionsProvider

---
+++ de-adito-blueprint-api/src/main/java/de/adito/nbm/blueprints/api/IBlueprintActionsProvider.java
@@ -0,0 +1,22 @@
+package de.adito.nbm.blueprints.api;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * Provider for Blueprint-Actions with specific types
+ *
+ * @author s.seemann, 11.12.2020
+ */
+public interface IBlueprintActionsProvider
+{
+
+  /**
+   * Returns an instance of the "create"-ActionGroup
+   *
+   * @param pTypes the name of the types of the blueprints, which should be added to the ActionGroup
+   * @return the ActionGroup
+   */
+  Action createModelActionGroup(@NotNull String... pTypes);
+}
