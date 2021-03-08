d94ac1d6de0e22871d54c5170850b51203c5e87b
m.kaspera <m.kaspera@adito.de>
2021-03-08 10:21:22 +0100

    Feat: added a method for setting the CloudId to the ISystemInfo interface

--- de-adito-runconfig-api/src/main/java/de/adito/nbm/runconfig/api/ISystemInfo.java
+++ de-adito-runconfig-api/src/main/java/de/adito/nbm/runconfig/api/ISystemInfo.java
@@ -2,6 +2,7 @@ package de.adito.nbm.runconfig.api;
 
 import de.adito.aditoweb.nbm.nbide.nbaditointerface.tunnel.ITunnelConfigProvider;
 import io.reactivex.rxjava3.core.Observable;
+import org.jetbrains.annotations.NotNull;
 import org.netbeans.api.project.Project;
 
 import java.util.Map;
@@ -21,8 +22,6 @@ public interface ISystemInfo extends ITunnelConfigProvider
   String CLOUD_ID_PREF_KEY_PEFIX = "ssp.cloudSystemId.";
 
   /**
-   *
-   *
    * @return true if the system is declared as productive system
    */
   boolean isProductive();
@@ -33,6 +32,13 @@ public interface ISystemInfo extends ITunnelConfigProvider
    * @return Observable
    */
   Observable<String> getCloudId();
+
+  /**
+   * Sets the CloudId for the current system, making it a cloud system in the process
+   *
+   * @param pCloudId the CloudId to be set
+   */
+  void setCloudId(@NotNull String pCloudId);
 
   /**
    * Get a map with parameter settings
