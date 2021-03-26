79b953e2a18d56a4f5dfa9e97f703ce6a5150993
Werner Glanzer <w.glanzer@adito.de>
2021-03-26 08:22:58 +0100

    feat: added "PreferencesObservable" to listen on preference changes

---
+++ de-adito-netbeans-utilities/src/main/java/de/adito/observables/netbeans/PreferencesObservable.java
@@ -0,0 +1,45 @@
+package de.adito.observables.netbeans;
+
+import de.adito.util.reactive.AbstractListenerObservable;
+import io.reactivex.rxjava3.core.Observable;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.prefs.*;
+
+
+/**
+ * Observable, that listens on preferences
+ *
+ * @author w.glanzer, 26.03.2021
+ */
+public class PreferencesObservable extends AbstractListenerObservable<PreferenceChangeListener, Preferences, Preferences>
+{
+
+  @NotNull
+  public static Observable<Preferences> create(@NotNull Preferences pPreferences)
+  {
+    return Observable.create(new PreferencesObservable(pPreferences))
+        .startWithItem(pPreferences);
+  }
+
+  private PreferencesObservable(@NotNull Preferences pBase)
+  {
+    super(pBase);
+  }
+
+  @NotNull
+  @Override
+  protected PreferenceChangeListener registerListener(@NotNull Preferences pBase, @NotNull IFireable<Preferences> pFireable)
+  {
+    PreferenceChangeListener pcl = ev -> pFireable.fireValueChanged(pBase);
+    pBase.addPreferenceChangeListener(pcl);
+    return pcl;
+  }
+
+  @Override
+  protected void removeListener(@NotNull Preferences pBase, @NotNull PreferenceChangeListener pPreferencesChangeListener)
+  {
+    pBase.removePreferenceChangeListener(pPreferencesChangeListener);
+  }
+
+}
