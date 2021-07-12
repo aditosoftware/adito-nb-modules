26a46bf1164175c34c7b9fc8f37dd4224fdf75dd
Werner Glanzer <w.glanzer@adito.de>
2021-07-12 11:21:35 +0200

    feat: moved old metric facade to new metric module

---
+++ de-adito-metrics-api/src/main/java/de/adito/aditoweb/nbm/metrics/api/IMetricProxyFactory.java
@@ -0,0 +1,54 @@
+package de.adito.aditoweb.nbm.metrics.api;
+
+import org.jetbrains.annotations.*;
+import org.openide.util.Lookup;
+
+/**
+ * This proxy factory creates a "shallow" wrapper objects around a given object, so that
+ * all metric annotations can be evaluated correctly.
+ *
+ * @author w.glanzer, 09.07.2021
+ */
+public interface IMetricProxyFactory
+{
+
+  /**
+   * Creates a "shallow" wrapper around a given object, so that all metric
+   * annotations can be evaluated correctly.
+   *
+   * @param pObject Object to proxy
+   * @return the proxied object that has to be used, if metrics should be evaluated
+   */
+  @Contract("null -> null")
+  static <T> T proxy(@Nullable T pObject)
+  {
+    if(pObject == null)
+      return null;
+
+    for (IMetricProxyFactory proxy : Lookup.getDefault().lookupAll(IMetricProxyFactory.class))
+    {
+      if(proxy.canCreateProxy(pObject))
+        return proxy.createProxy(pObject);
+    }
+
+    throw new IllegalArgumentException("Failed to find appropriate proxy for object " + pObject + " " + pObject.getClass());
+  }
+
+  /**
+   * Specifies, if this factory can create a proxy object of the given parameter
+   *
+   * @param pObject Object to proxy
+   * @return true, if it can be proxied by this proxy factory
+   */
+  boolean canCreateProxy(@NotNull Object pObject);
+
+  /**
+   * Creates a proxy around the given object
+   *
+   * @param pObject Object to proxy
+   * @return the proxied object
+   */
+  @NotNull
+  <T> T createProxy(@NotNull T pObject);
+
+}
---
+++ de-adito-metrics-api/src/main/java/de/adito/aditoweb/nbm/metrics/api/types/Counted.java
@@ -0,0 +1,12 @@
+package de.adito.aditoweb.nbm.metrics.api.types;
+
+import java.lang.annotation.*;
+
+/**
+ * @author w.glanzer, 08.07.2021
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Counted
+{
+}
