470ce9806f593390513e6719b1b53f98a26f4eed
Werner Glanzer <w.glanzer@adito.de>
2021-07-14 11:47:38 +0200

    feat: added metric type "Gauge"

---
+++ de-adito-metrics-api/src/main/java/de/adito/aditoweb/nbm/metrics/api/types/Gauge.java
@@ -0,0 +1,23 @@
+package de.adito.aditoweb.nbm.metrics.api.types;
+
+import java.lang.annotation.*;
+
+/**
+ * A gauge is the simplest metric type. It just returns a value, so that the exporter will export the given value.
+ *
+ * The method annotated with @Gauge has to return the desired value, NULL is valid.
+ *
+ * @author w.glanzer, 14.07.2021
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@MetricType
+public @interface Gauge
+{
+
+  /**
+   * @return unique name of this metric. Must not be empty and has to be unique!
+   */
+  String name();
+
+}
