717a788cd22b1a532286b9f014900dc00caf0eea
Werner Glanzer <w.glanzer@adito.de>
2021-07-14 10:20:51 +0200

    doc: added documenation for @Counted

--- de-adito-metrics-api/src/main/java/de/adito/aditoweb/nbm/metrics/api/types/Counted.java
+++ de-adito-metrics-api/src/main/java/de/adito/aditoweb/nbm/metrics/api/types/Counted.java
@@ -3,6 +3,13 @@ package de.adito.aditoweb.nbm.metrics.api.types;
 import java.lang.annotation.*;
 
 /**
+ * All method invocations of methods annotated with @Counted will be counted, so that
+ * at the end of the day you get the amount of method calls during the whole application session.
+ *
+ * The counter never gets decreased, only increased on method call.
+ *
+ * The return type of the annotated method won't be used and does not effect the metric collection.
+ *
  * @author w.glanzer, 08.07.2021
  */
 @Retention(RetentionPolicy.RUNTIME)
