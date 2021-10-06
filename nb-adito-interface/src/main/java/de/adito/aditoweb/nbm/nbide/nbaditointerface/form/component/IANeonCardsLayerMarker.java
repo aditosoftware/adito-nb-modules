8f31ba2eacd5893ba8ca39c7cb9bbf9a1b99c846
Werner Glanzer <w.glanzer@adito.de>
2021-10-06 13:03:35 +0200

    feat: removed module "org-netbeans-modules-form" and its interfaces in "nb-adito-interface"

--- nb-adito-interface/src/main/java/de/adito/aditoweb/nbm/nbide/nbaditointerface/form/component/IANeonCardsLayerMarker.java
+++
@@ -1,9 +0,0 @@
-package de.adito.aditoweb.nbm.nbide.nbaditointerface.form.component;
-
-/**
- * Markierungsinterface für ein ANeonCardsLayer Objekt.
- * @author Thomas Tasior 08.02.2017, 07:57
- */
-public interface IANeonCardsLayerMarker
-{
-}
