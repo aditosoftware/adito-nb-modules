48f2214a63d533f7b4c36e6136e1761287184779
Werner Glanzer <w.glanzer@adito.de>
2021-12-17 12:10:42 +0100

    feat: added ADITOLinkWindowsNatives to read symbolic links under windows

---
+++ org-netbeans-modules-masterfs/src/main/java/org/netbeans/modules/masterfs/watcher/ADITOLinkWindowsNatives.java
@@ -0,0 +1,158 @@
+package org.netbeans.modules.masterfs.watcher;
+
+import org.jetbrains.annotations.NotNull;
+import org.openide.util.*;
+import sun.misc.Unsafe;
+
+import java.io.*;
+import java.lang.reflect.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.logging.*;
+
+/**
+ * @author w.glanzer, 17.12.2021
+ */
+class ADITOLinkWindowsNatives
+{
+
+  private static Unsafe _UNSAFE;
+  private static Method _IS_REPARSE_POINT;
+  private static Method _OPEN_FOR_READATTRIBUTE_ACCESS;
+  private static Method _GET_NATIVE_BUFFER;
+  private static Method _GET_NATIVE_BUFFER_ADDRESS;
+  private static Method _GET_NATIVE_BUFFER_RELEASE;
+  private static Method _DEVICEIO_CONTROL_GET_REPARSE_POINT;
+  private static Method _STRIP_PREFIX;
+
+  static
+  {
+    try
+    {
+      Class<?> unsafe = Unsafe.class;
+      Field theUnsafe = unsafe.getDeclaredField("theUnsafe");
+      theUnsafe.setAccessible(true);
+      _UNSAFE = (Unsafe) theUnsafe.get(null);
+
+      _IS_REPARSE_POINT = Class.forName("sun.nio.fs.WindowsFileAttributes").getDeclaredMethod("isReparsePoint");
+      _IS_REPARSE_POINT.setAccessible(true);
+
+      _OPEN_FOR_READATTRIBUTE_ACCESS = Class.forName("sun.nio.fs.WindowsPath").getDeclaredMethod("openForReadAttributeAccess", boolean.class);
+      _OPEN_FOR_READATTRIBUTE_ACCESS.setAccessible(true);
+
+      _GET_NATIVE_BUFFER = Class.forName("sun.nio.fs.NativeBuffers").getDeclaredMethod("getNativeBuffer", int.class);
+      _GET_NATIVE_BUFFER.setAccessible(true);
+
+      Class<?> nativeBuffer = Class.forName("sun.nio.fs.NativeBuffer");
+      _GET_NATIVE_BUFFER_ADDRESS = nativeBuffer.getDeclaredMethod("address");
+      _GET_NATIVE_BUFFER_ADDRESS.setAccessible(true);
+      _GET_NATIVE_BUFFER_RELEASE = nativeBuffer.getDeclaredMethod("release");
+      _GET_NATIVE_BUFFER_RELEASE.setAccessible(true);
+
+      _DEVICEIO_CONTROL_GET_REPARSE_POINT = Class.forName("sun.nio.fs.WindowsNativeDispatcher")
+          .getDeclaredMethod("DeviceIoControlGetReparsePoint", long.class, long.class, int.class);
+      _DEVICEIO_CONTROL_GET_REPARSE_POINT.setAccessible(true);
+
+      _STRIP_PREFIX = Class.forName("sun.nio.fs.WindowsLinkSupport").getDeclaredMethod("stripPrefix", String.class);
+      _STRIP_PREFIX.setAccessible(true);
+    }
+    catch (Exception e)
+    {
+      // only log on Windows, because unix/mac won't have this class.
+      if (BaseUtilities.isWindows())
+        Logger.getLogger(ADITOWatcherSymlinkExt.class.getName()).log(Level.WARNING, "", e);
+    }
+  }
+
+  /**
+   * Determines, if the given path is a junction link
+   *
+   * @param pPath path to determine
+   * @return true, if it is a junction link
+   */
+  public static boolean isJunctionLink(@NotNull String pPath) throws Exception
+  {
+    BasicFileAttributes attributes = Files.readAttributes(new File(pPath).toPath(), BasicFileAttributes.class, LinkOption.NOFOLLOW_LINKS);
+    return (boolean) _IS_REPARSE_POINT.invoke(attributes);
+  }
+
+  /**
+   * Reads the target of the given junction link
+   *
+   * @param pPath Path to read its target from
+   * @return the target
+   */
+  @NotNull
+  public static String readJunctionLink(@NotNull String pPath) throws Exception
+  {
+    assert _OPEN_FOR_READATTRIBUTE_ACCESS != null && _IS_REPARSE_POINT != null &&
+        _GET_NATIVE_BUFFER != null && _GET_NATIVE_BUFFER_ADDRESS != null && _GET_NATIVE_BUFFER_RELEASE != null &&
+        _DEVICEIO_CONTROL_GET_REPARSE_POINT != null && _STRIP_PREFIX != null;
+    long pathHandle = (long) _OPEN_FOR_READATTRIBUTE_ACCESS.invoke(new File(pPath).toPath(), false);
+    int bufferSize = 16 * 1024;
+    Object nativeBuffer = _GET_NATIVE_BUFFER.invoke(null, bufferSize);
+
+    try
+    {
+      // Read all Information into nativeBuffer
+      _DEVICEIO_CONTROL_GET_REPARSE_POINT.invoke(null, pathHandle, _GET_NATIVE_BUFFER_ADDRESS.invoke(nativeBuffer), bufferSize);
+
+      // The native buffer should now contain a struct _REPARSE_DATA_BUFFER
+      // typedef struct _REPARSE_DATA_BUFFER {
+      //     ULONG  ReparseTag;
+      //     USHORT  ReparseDataLength;
+      //     USHORT  Reserved;
+      //     union {
+      //         struct {
+      //             USHORT  SubstituteNameOffset;
+      //             USHORT  SubstituteNameLength;
+      //             USHORT  PrintNameOffset;
+      //             USHORT  PrintNameLength;
+      //             WCHAR  PathBuffer[1];
+      //         } SymbolicLinkReparseBuffer;
+      //         struct {
+      //             USHORT  SubstituteNameOffset;
+      //             USHORT  SubstituteNameLength;
+      //             USHORT  PrintNameOffset;
+      //             USHORT  PrintNameLength;
+      //             WCHAR  PathBuffer[1];
+      //         } MountPointReparseBuffer;
+      //         struct {
+      //             UCHAR  DataBuffer[1];
+      //         } GenericReparseBuffer;
+      //     };
+      // } REPARSE_DATA_BUFFER
+      //
+
+      int IO_REPARSE_TAG_MOUNTPOINT = 0xA0000003;
+      short OFFSETOF_REPARSETAG = 0;
+      short OFFSETOF_PATHOFFSET = 8;
+      short OFFSETOF_PATHLENGTH = 10;
+      short OFFSETOF_MOUNTPOINT_REPARSE_BUFFER = 16;
+
+      long nativeBufferAddress = (long) _GET_NATIVE_BUFFER_ADDRESS.invoke(nativeBuffer);
+      int tag = (int) _UNSAFE.getLong(nativeBufferAddress + OFFSETOF_REPARSETAG);
+      if (tag != IO_REPARSE_TAG_MOUNTPOINT)
+        throw new NotLinkException(pPath, null, "Reparse point is not a mount point -> no junction");
+
+      short nameOffset = _UNSAFE.getShort(nativeBufferAddress + OFFSETOF_PATHOFFSET);
+      short nameLengthInBytes = _UNSAFE.getShort(nativeBufferAddress + OFFSETOF_PATHLENGTH);
+      if ((nameLengthInBytes % 2) != 0)
+        throw new FileSystemException(pPath, null, "link corrupted");
+
+      char[] link = new char[nameLengthInBytes / 2];
+      _UNSAFE.copyMemory(null, nativeBufferAddress + OFFSETOF_MOUNTPOINT_REPARSE_BUFFER + nameOffset, link,
+                         Unsafe.ARRAY_CHAR_BASE_OFFSET, nameLengthInBytes);
+
+      String target = (String) _STRIP_PREFIX.invoke(null, new String(link));
+      if (target.isEmpty())
+        throw new IOException(pPath + " link target invalid");
+      return target;
+    }
+    finally
+    {
+      _GET_NATIVE_BUFFER_RELEASE.invoke(nativeBuffer);
+    }
+  }
+
+}
--- org-netbeans-modules-masterfs/src/main/java/org/netbeans/modules/masterfs/watcher/ADITOWatcherSymlinkExt.java
+++ org-netbeans-modules-masterfs/src/main/java/org/netbeans/modules/masterfs/watcher/ADITOWatcherSymlinkExt.java
@@ -4,19 +4,35 @@ import org.jetbrains.annotations.*;
 import org.netbeans.modules.masterfs.filebasedfs.fileobjects.FileObjectFactory;
 import org.netbeans.modules.masterfs.providers.Notifier;
 import org.openide.filesystems.*;
+import org.openide.util.BaseUtilities;
 
 import java.io.File;
 import java.util.*;
 
 /**
+ * Implementes specialhandling for NTFS junction files
+ * and "regular" symlink files under linux
+ *
  * @author w.glanzer, 16.12.2021
  */
 class ADITOWatcherSymlinkExt
 {
 
+  /**
+   * Returns all references that have to be refreshed, after pChangedFile changed.
+   * This method tries to handle symbolic links too.
+   *
+   * @param pChangedFile       File that changed
+   * @param pFileObjectFactory Factory to retrieve FileObjects
+   * @param pWatchedRefs       All Refs, that are currently watched
+   * @param pNotifier          Notifier
+   * @return a set of FileObjects that have to be recalculcated / refreshed, because they somehow belong to pChangedFile
+   */
+  @NotNull
   public static <KEY> Set<FileObject> getAllReferences(@NotNull File pChangedFile, @NotNull FileObjectFactory pFileObjectFactory,
                                                        @NotNull Set<NotifierKeyRef> pWatchedRefs, @NotNull Notifier<KEY> pNotifier)
   {
+    // NetBeans Original
     Set<FileObject> toRefresh = new HashSet<>();
     FileObject fo = pFileObjectFactory.getCachedOnly(pChangedFile);
     if (fo == null || fo.isData())
@@ -27,30 +43,59 @@ class ADITOWatcherSymlinkExt
       NotifierKeyRef<?> kr = new NotifierKeyRef<>(fo, null, null, pNotifier);
       if (pWatchedRefs.contains(kr))
         toRefresh.add(fo);
+    }
 
-      //todo the "fo" does not have to be cached explicitly - there may be only symlinks without fo
-      try
+    // ADITO: Retrieve all symlinked files
+    try
+    {
+      String pathToFire = fo == null ? pChangedFile.getAbsolutePath() : fo.getPath();
+      for (NotifierKeyRef<?> watchedRef : pWatchedRefs)
       {
-        for (NotifierKeyRef watchedRef : pWatchedRefs)
+        FileObject refFo = watchedRef.get();
+        if (refFo != null && _isSymbolicLinkRecursive(refFo))
         {
-          FileObject refFo = watchedRef.get();
-          if (refFo != null && _isSymbolicLinkRecursive(refFo)) //only check links
-          {
-            FileObject symlink = _readSymbolicLinkRecursive(refFo);
-            if (Objects.equals(fo, symlink))
-              toRefresh.add(refFo);
-          }
+          FileObject symlinkTarget = _readSymbolicLinkRecursive(refFo);
+          if (Objects.equals(pathToFire, symlinkTarget.getPath()))
+            toRefresh.add(refFo);
         }
       }
-      catch (Exception e)
-      {
-        e.printStackTrace(); //todo
-      }
+    }
+    catch (Exception e)
+    {
+      // ignore
     }
 
     return toRefresh;
   }
 
+  /**
+   * Determines, if pFo is a symbolic link.
+   * This method returns true, even if a parent is a symbolic link.
+   *
+   * @param pFo FileObject to check
+   * @return true if pFo or any of its parents are symlinks
+   */
+  private static boolean _isSymbolicLinkRecursive(@Nullable FileObject pFo) throws Exception
+  {
+    FileObject fo = pFo;
+    while (fo != null)
+    {
+      if (_isSymbolicLink(fo))
+        return true;
+      fo = fo.getParent();
+    }
+    return false;
+  }
+
+  /**
+   * Reads the target of the symlink which pFo references.
+   * This method tries to resolve the whole symlink path of pFo, so
+   * if some parents of pFo are symlinks, they get resolved too.
+   *
+   * @param pFo FileObject to check
+   * @return the target
+   */
+  @NotNull
   private static FileObject _readSymbolicLinkRecursive(@NotNull FileObject pFo) throws Exception
   {
     FileObject fo = pFo;
@@ -69,26 +114,58 @@ class ADITOWatcherSymlinkExt
     throw new NullPointerException(pFo.toString());
   }
 
-  private static boolean _isSymbolicLinkRecursive(@Nullable FileObject pFo) throws Exception
-  {
-    FileObject fo = pFo;
-    while (fo != null)
-    {
-      if (_isSymbolicLink(fo))
-        return true;
-      fo = fo.getParent();
-    }
-    return false;
-  }
-
+  /**
+   * Determines, if pFo (and only pFo, no parent check!) is a symlink.
+   * This has to be done a little weird, because of NTFS "Junction" links.
+   * Those are symlinks too, but {@link FileObject#isSymbolicLink()} returns false, because
+   * symlinks are handeld different in NTFS.
+   *
+   * @param pFo FileObject to check
+   * @return true, if pFo (and only pFo!) is a kind of symlink
+   */
   private static boolean _isSymbolicLink(@NotNull FileObject pFo) throws Exception
   {
+    try
+    {
+      // Specialhandling: NTFS - only on Windows
+      if (BaseUtilities.isWindows())
+        if (ADITOLinkWindowsNatives.isJunctionLink(pFo.getPath()))
+          return true;
+    }
+    catch (Exception e)
+    {
+      // fallback
+    }
+
     return pFo.isSymbolicLink();
   }
 
+  /**
+   * Reads the target of the symlink which pFo references.
+   * This has to be done a little weird, because of NTFS "Junction" links.
+   * Those are symlinks too, but {@link FileObject#isSymbolicLink()} returns false, because
+   * symlinks are handeld different in NTFS. So {@link FileObject#readSymbolicLink()} will throw an exception,
+   * because NetBeans thinks, that they are no symbolic links...
+   *
+   * @param pFo FileObject to check
+   * @return the target
+   */
   private static FileObject _readSymbolicLink(@NotNull FileObject pFo) throws Exception
   {
+    try
+    {
+      // Specialhandling: NTFS - only on Windows
+      if (BaseUtilities.isWindows())
+        if (ADITOLinkWindowsNatives.isJunctionLink(pFo.getPath()))
+          return FileUtil.toFileObject(new File(ADITOLinkWindowsNatives.readJunctionLink(pFo.getPath())));
+    }
+    catch (Exception e)
+    {
+      // fallback
+    }
+
     return pFo.readSymbolicLink();
   }
+
 
 }
