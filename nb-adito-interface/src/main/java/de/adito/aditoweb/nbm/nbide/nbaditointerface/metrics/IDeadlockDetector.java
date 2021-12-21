35e0f3c6f9e7be3f9b15c44e79226adf0f4e4e76
m.kaspera <m.kaspera@adito.de>
2021-12-21 07:04:39 +0100

    Feat: Added an interface for a deadlock detector that runs in the background and uses the metrics framework of the designer to log deadlocks

---
+++ nb-adito-interface/src/main/java/de/adito/aditoweb/nbm/nbide/nbaditointerface/metrics/IDeadlockDetector.java
@@ -0,0 +1,17 @@
+package de.adito.aditoweb.nbm.nbide.nbaditointerface.metrics;
+
+/**
+ * A deadlockDetector should detect deadlocks in the application threads. As such, the detection of the deadlocks should run in its own separate
+ * thread so that there is no possibility of the deadlock affecting the deadlockDetecor itself
+ *
+ * @author m.kaspera, 15.12.2021
+ */
+public interface IDeadlockDetector
+{
+
+  /**
+   * starts the deadlockDetector, this should start its own Thread or Executor in which the deadlocks are detected and then immediately return
+   */
+  void start();
+
+}
