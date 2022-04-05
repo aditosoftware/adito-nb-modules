06027994b06c0165af9e5fbc6d0dc1ed26f40439
Werner Glanzer <w.glanzer@adito.de>
2022-04-05 13:36:24 +0200

    feat: added "createForPlainFile" to FileObservable

--- de-adito-netbeans-utilities/src/main/java/de/adito/observables/netbeans/FileObservable.java
+++ de-adito-netbeans-utilities/src/main/java/de/adito/observables/netbeans/FileObservable.java
@@ -28,11 +28,54 @@ public class FileObservable extends AbstractListenerObservable<FileChangeListene
    * @return Observable
    */
   @NotNull
+  @Deprecated
   public static Observable<Optional<File>> create(@NotNull File pFile)
+  {
+    return createForDirectory(pFile);
+  }
+
+  /**
+   * Creates a new observable that contains the file object, if the given folder exists.
+   * Returns an empty optional, if it does not.
+   *
+   * @param pFile File to check
+   * @return Observable
+   */
+  @NotNull
+  public static Observable<Optional<File>> createForDirectory(@NotNull File pFile)
   {
     return Observables.create(new FileObservable(pFile), () -> _getValue(pFile))
         .observeOn(Schedulers.io())
         .subscribeOn(Schedulers.io());
+  }
+
+  /**
+   * Creates a new observable that contains the file object, if the given file exists.
+   * Returns an empty optional, if it does not.
+   *
+   * @param pFile File to check
+   * @return Observable
+   */
+  @NotNull
+  public static Observable<Optional<File>> createForPlainFile(@NotNull File pFile)
+  {
+    return createForDirectory(pFile.getParentFile())
+
+        // determine, if File is available in parent and distinct it afterwards, so we will only be triggered initially and on file creation / deletion
+        .map(pParent -> _getValue(pFile))
+        .distinctUntilChanged()
+
+        // handle file changes, if pMyFileOpt is available in parent
+        .switchMap(pMyFileOpt -> pMyFileOpt
+            .map(pMyFile -> Observables.create(new _NonRecursiveFileObservable(pMyFile), () -> _getValue(pMyFile)))
+            .orElseGet(() -> Observable.just(Optional.empty())))
+
+        // distinct, if file has not really changed
+        .map(pFileOpt -> pFileOpt
+            .map(File::lastModified)
+            .orElse(-1L))
+        .distinctUntilChanged()
+        .map(pL -> pL == -1 ? Optional.empty() : _getValue(pFile));
   }
 
   @NotNull
@@ -63,6 +106,39 @@ public class FileObservable extends AbstractListenerObservable<FileChangeListene
     if(pFile.exists())
       return Optional.of(pFile);
     return Optional.empty();
+  }
+
+  /**
+   * FileObservable, but not recursive
+   */
+  private static class _NonRecursiveFileObservable extends AbstractListenerObservable<FileChangeListener, File, Optional<File>>
+  {
+    public _NonRecursiveFileObservable(@NotNull File pListenableValue)
+    {
+      super(pListenableValue);
+    }
+
+    @NotNull
+    @Override
+    protected FileChangeListener registerListener(@NotNull File pFile, @NotNull IFireable<Optional<File>> pFireable)
+    {
+      FileChangeListener fcl = new _FileListenerImpl(pFireable, pFile);
+      FileUtil.addFileChangeListener(fcl, FileUtil.normalizeFile(pFile));
+      return fcl;
+    }
+
+    @Override
+    protected void removeListener(@NotNull File pFile, @NotNull FileChangeListener pFileChangeListener)
+    {
+      try
+      {
+        FileUtil.removeFileChangeListener(pFileChangeListener, FileUtil.normalizeFile(pFile));
+      }
+      catch(IllegalArgumentException iae)
+      {
+        // ignore
+      }
+    }
   }
 
   private static class _FileListenerImpl implements FileChangeListener
