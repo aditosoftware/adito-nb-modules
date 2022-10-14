4ed010d7b620d63c436fb74229c315a082c7d3c3
Pascal Neub <p.neub@adito.de>
2022-10-14 09:12:02 +0200

    feat: added INodeJSExecutor.executeSync overload, to specify if the stderr should be included in the output

--- nb-adito-interface/src/main/java/de/adito/aditoweb/nbm/nbide/nbaditointerface/javascript/node/INodeJSExecutor.java
+++ nb-adito-interface/src/main/java/de/adito/aditoweb/nbm/nbide/nbaditointerface/javascript/node/INodeJSExecutor.java
@@ -31,15 +31,32 @@ public interface INodeJSExecutor
    *
    * @param pEnv     Environment to execute on. Has to be valid.
    * @param pBase    Base to execute commands on
-   * @param pParams  Command to execute, without the base prefix
    * @param pTimeout Timeout in ms, -1 if no timeout
+   * @param pParams  Command to execute, without the base prefix
    * @return the output of the executed nodejs command
-   * @throws IOException          if an error occured
+   * @throws IOException          if an error occurred
    * @throws InterruptedException if the timeout killed the process
    * @throws TimeoutException     if the timeout killed the process
    */
   @NotNull
   String executeSync(@NotNull INodeJSEnvironment pEnv, @NotNull INodeJSExecBase pBase, long pTimeout, @NotNull String... pParams)
+      throws IOException, InterruptedException, TimeoutException;
+
+  /**
+   * Executes the given command on the nodejs environment and waits until it finished
+   *
+   * @param pEnv           Environment to execute on. Has to be valid.
+   * @param pBase          Base to execute commands on
+   * @param pTimeout       Timeout in ms, -1 if no timeout
+   * @param pIncludeStdErr Whether to include STDERR in the output
+   * @param pParams        Command to execute, without the base prefix
+   * @return the output of the executed nodejs command
+   * @throws IOException          if an error occurred
+   * @throws InterruptedException if the timeout killed the process
+   * @throws TimeoutException     if the timeout killed the process
+   */
+  @NotNull
+  String executeSync(@NotNull INodeJSEnvironment pEnv, @NotNull INodeJSExecBase pBase, long pTimeout, boolean pIncludeStdErr, @NotNull String... pParams)
       throws IOException, InterruptedException, TimeoutException;
 
   /**
@@ -49,7 +66,7 @@ public interface INodeJSExecutor
    * @param pBase   Base to execute commands on
    * @param pParams Command to execute, without the base prefix
    * @return the process handle
-   * @throws IOException if an error occured
+   * @throws IOException if an error occurred
    */
   @NotNull
   Process execute(@NotNull INodeJSEnvironment pEnv, @NotNull INodeJSExecBase pBase, @NotNull String... pParams) throws IOException;
@@ -64,7 +81,7 @@ public interface INodeJSExecutor
    * @param pDefaultIn  Default-Input, NULL ignores the input and does not delegate something to the spawned process
    * @param pParams     Command to execute, without the base prefix
    * @return the future containing the exitcode
-   * @throws IOException if an error occured
+   * @throws IOException if an error occurred
    */
   @NotNull
   CompletableFuture<Integer> executeAsync(@NotNull INodeJSEnvironment pEnv, @NotNull INodeJSExecBase pBase,
