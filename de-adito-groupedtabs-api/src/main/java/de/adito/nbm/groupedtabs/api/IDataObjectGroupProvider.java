6e38d8e1bec225ea77591d13ed39468049fc66d5
Pascal Neub <p.neub@adito.de>
2023-03-02 13:05:50 +0100

    feat: add de-adito-groupedtabs-api

---
+++ de-adito-groupedtabs-api/src/main/java/de/adito/nbm/groupedtabs/api/IDataObjectGroupProvider.java
@@ -0,0 +1,38 @@
+package de.adito.nbm.groupedtabs.api;
+
+import org.jetbrains.annotations.*;
+import org.openide.loaders.DataObject;
+import org.openide.util.Lookup;
+
+import java.util.*;
+
+/**
+ * Provide an implementation of this interface in the global lookup to alter the grouping behavior of DataObject tabs.
+ * Implementations also have to implement a Comparator for DataObjects, to sort DataObjects inside groups.
+ *
+ * @author p.neub, 27.02.2023
+ */
+public interface IDataObjectGroupProvider extends Comparator<DataObject>
+{
+  /**
+   * Returns an instance of the {@link IDataObjectGroupProvider}.
+   * The default implementation is {@link de.adito.nbm.groupedtabs.impl.DefaultDataObjectGroupProvider}
+   *
+   * @return an instance of {@link IDataObjectGroupProvider}
+   */
+  @NotNull
+  static IDataObjectGroupProvider getDefault()
+  {
+    return Objects.requireNonNull(Lookup.getDefault().lookup(IDataObjectGroupProvider.class));
+  }
+
+  /**
+   * Name of the group that the specified DataObject is part of.
+   * Return {@link Optional#empty()} if this DataObject does not belong to any specific group.
+   *
+   * @param pDataObject the DataObject
+   * @return the group of the DataObject
+   */
+  @NotNull
+  Optional<String> group(@NotNull DataObject pDataObject);
+}
