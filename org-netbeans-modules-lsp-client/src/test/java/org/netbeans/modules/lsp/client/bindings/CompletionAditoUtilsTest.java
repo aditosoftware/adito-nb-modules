7d9e3522bdaf961db02e3924f3bf00f2df1b8c75
Werner Glanzer <w.glanzer@adito.de>
2023-09-13 11:16:52 +0200

    fix: autocomplete should not insert a "$1" suffix in import statements (#2024730)

--- org-netbeans-modules-lsp-client/src/test/java/org/netbeans/modules/lsp/client/bindings/CompletionAditoUtilsTest.java
+++ org-netbeans-modules-lsp-client/src/test/java/org/netbeans/modules/lsp/client/bindings/CompletionAditoUtilsTest.java
@@ -1,9 +1,14 @@
 package org.netbeans.modules.lsp.client.bindings;
 
+import lombok.NonNull;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
 import org.netbeans.modules.editor.NbEditorDocument;
 
 import javax.swing.text.*;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,6 +18,35 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class CompletionAditoUtilsTest
 {
+
+  /**
+   * @return Arguments for the parameterized test method {@link CompletionAditoUtilsTest#testRemoveInvalidSuffixes(String, String)}
+   */
+  @NonNull
+  private static Stream<Arguments> testRemoveInvalidSuffixes()
+  {
+    return Stream.of(
+        Arguments.of("import { Test } from \"dummy\"", "import { Test$1 } from \"dummy\""),
+        Arguments.of("import { T, Test } from \"dummy\"", "import { T, Test$1 } from \"dummy\""),
+        Arguments.of("import { T, Test, T2 } from \"dummy\"", "import { T, Test$1, T2 } from \"dummy\""),
+        Arguments.of("import { Test } from \"dummy\"", "import { Test } from \"dummy\""),
+        Arguments.of("import { T$1est } from \"dummy\"", "import { T$1est } from \"dummy\""),
+        Arguments.of("import { T, T$1est } from \"dummy\"", "import { T, T$1est } from \"dummy\"")
+    );
+  }
+
+  /**
+   * Tests, if invalid suffixes get removed
+   *
+   * @param pExpected Already replaced string, that is the expected result of {@link CompletionAditoUtils#removeInvalidSuffixes(String)}
+   * @param pToTest   String to test the replacement
+   */
+  @MethodSource
+  @ParameterizedTest
+  void testRemoveInvalidSuffixes(@NonNull String pExpected, @NonNull String pToTest)
+  {
+    assertEquals(pExpected, CompletionAditoUtils.removeInvalidSuffixes(pToTest));
+  }
 
   /**
    * Tests that the {@code @} is correctly detected and removed.
